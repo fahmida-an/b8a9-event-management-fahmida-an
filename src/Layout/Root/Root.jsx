716100bd0a58194236fa87c8303d5651ca9b2ca1
Fahmida Khan <fahmidakhann.19@gmail.com>
2023-10-07 19:20:08 +0600

    Custom font and colors added

--- src/Layout/Root/Root.jsx
+++ src/Layout/Root/Root.jsx
@@ -3,7 +3,7 @@ import { Outlet } from "react-router-dom";
 
 const Root = () => {
     return (
-        <div>
+        <div className="max-w-screen-xl mx-auto font-roboto">
             <Outlet></Outlet>
         </div>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,25 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    colors: {
+      pinkbright1: '#f9a8d4',
+      pinkdark1: '#9d174d',
+      pinkdark2: '#831843',
+      dark1: '#706F6F',
+      dark2: '#403F3F',
+      dark3: '#9F9F9F',
+      pink1: '#D72050',
+      white: '#FFFFFF',
+      gray: '#F3F3F3',
+      blue: '#1fb6ff',
+      black: '#000000',
+
+     },
+    extend: {
+      fontFamily: {
+        roboto: "'Roboto', sans-serif"
+      }
+    },
   },
   plugins: [require("daisyui")],
 }
